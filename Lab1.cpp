693bd8884b8a842c18daef7f4f1fa52a67012232
WoonHak Kong <woonhak.kong@georgebrown.ca>
2021-01-20 03:53:34 +0900

    test commit

---
+++ Lab1.cpp
@@ -0,0 +1,19 @@
+// Lab1 Exercise
+// WoonHak kong - 101300258
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+
+	cout << "Testing..." << endl;
+
+
+
+
+
+	return 0;
+}
